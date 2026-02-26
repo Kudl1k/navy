2f8075f58ceb488f10e4ed266dc877a705129fb8
Kudl1k <kudlacek@oksystem.cz>
2026-02-26 12:41:11 +0100

    Refactor Perceptron class methods for clarity and consistency

--- src/exec01/exec01.py
+++ src/exec01/exec01.py
@@ -4,37 +4,38 @@ import numpy as np
 class Perceptron:
 
     def __init__(self, value, l_r=0.1, n_iter=100):
-        self.iteration_count = None                                 # Number of iterations until convergence
-        self.l_r = l_r                                              # Learning rate
-        self.n_iter = n_iter                                        # Maximum number of iterations (Epochs)
-        self._set_weights(value)                                    # Initialize weights
+        self.weights = None                                         # vektor
+        self.iteration_count = None                                 # pocet iteraci, nez zkonverguje
+        self.l_r = l_r                                              # learning rate
+        self.n_iter = n_iter                                        # maximalni pocet iteraci
+        self.set_weights(value)                                    # inicializace vektoru
 
-    def _set_weights(self, n_features):
+    def set_weights(self, n_features):
         self.weights = np.random.randn(n_features.size + 1) * 0.01  # Initialize weights to small random values
 
     @staticmethod
-    def _activate(x):
+    def activate(x):
         if x > 0:
             return 1
         if x < 0:
             return -1
         return 0
 
-    def _predict(self, X):
-        z = np.dot(X, self.weights[1:]) + self.weights[0]           # Linear combination of inputs and weights
-        return self._activate(z)                                    # Activation function
+    def predict(self, X):
+        z = np.dot(X, self.weights[1:]) + self.weights[0]          # linearni kombinace vstupu a vah
+        return self.activate(z)                                    # aktivacni funkce
 
     def train(self, X, y):
         self.iteration_count = 0
         for _ in range(self.n_iter):
             predicted_all = True
             for xi, target in zip(X, y):
-                prediction = self._predict(xi)                      # Make a prediction
-                if prediction != target:                            # Check if it was wrong
+                prediction = self.predict(xi)                       # udelame predikci pro vzorek
+                if prediction != target:                            # pokud se predikce neshoduje s targetem, musime aktualizovat vektory vah
                     predicted_all = False
-                    update = self.l_r * (target - prediction)       # Calculate weight update
-                    self.weights[1:] += update * xi                 # Update weights
-                    self.weights[0] += update                       # Also update bias weight
+                    update = self.l_r * (target - prediction)       # vypocet aktualizace vah
+                    self.weights[1:] += update * xi                 # Aktualizace vah pro vstupni vektory
+                    self.weights[0] += update                       # Aktualizace vah pro bias
 
             if predicted_all:
                 break
